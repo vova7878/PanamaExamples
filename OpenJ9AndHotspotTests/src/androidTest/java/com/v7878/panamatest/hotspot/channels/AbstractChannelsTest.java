6a404b2a9eb94da0f4550a0276349c7d1608dad6
v7878 <46866705+vova7878@users.noreply.github.com>
2024-10-26 20:45:45 +0700

    Add TestAsyncSocketChannels and TestSocketChannels

---
+++ OpenJ9AndHotspotTests/src/androidTest/java/com/v7878/panamatest/hotspot/channels/AbstractChannelsTest.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright (c) 2021, 2023, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package com.v7878.panamatest.hotspot.channels;
+
+import static com.v7878.foreign.ValueLayout.JAVA_BYTE;
+import static org.junit.Assert.assertTrue;
+
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.v7878.foreign.Arena;
+import com.v7878.foreign.MemorySegment;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+/**
+ * Not a test, but infra for channel tests.
+ */
+public class AbstractChannelsTest {
+
+    static final Class<IOException> IOE = IOException.class;
+    static final Class<ExecutionException> EE = ExecutionException.class;
+    static final Class<IllegalStateException> ISE = IllegalStateException.class;
+
+    @FunctionalInterface
+    interface ThrowingConsumer<T, X extends Throwable> {
+        void accept(T action) throws X;
+    }
+
+    static long remaining(ByteBuffer[] buffers) {
+        return Arrays.stream(buffers).mapToLong(ByteBuffer::remaining).sum();
+    }
+
+    static ByteBuffer[] flip(ByteBuffer[] buffers) {
+        Arrays.stream(buffers).forEach(ByteBuffer::flip);
+        return buffers;
+    }
+
+    static ByteBuffer[] clear(ByteBuffer[] buffers) {
+        Arrays.stream(buffers).forEach(ByteBuffer::clear);
+        return buffers;
+    }
+
+    static final Random RANDOM = new Random(ThreadLocalRandom.current().nextInt());
+
+    static ByteBuffer segmentBufferOfSize(Arena session, int size) {
+        var segment = session.allocate(size, 1);
+        for (int i = 0; i < size; i++) {
+            segment.set(JAVA_BYTE, i, ((byte) RANDOM.nextInt()));
+        }
+        return segment.asByteBuffer();
+    }
+
+    static ByteBuffer[] segmentBuffersOfSize(int len, Arena session, int size) {
+        ByteBuffer[] bufs = new ByteBuffer[len];
+        for (int i = 0; i < len; i++)
+            bufs[i] = segmentBufferOfSize(session, size);
+        return bufs;
+    }
+
+    /**
+     * Returns an array of mixed source byte buffers; both heap and direct,
+     * where heap can be from the global session or session-less, and direct are
+     * associated with the given session.
+     */
+    static ByteBuffer[] mixedBuffersOfSize(int len, Arena session, int size) {
+        ByteBuffer[] bufs;
+        boolean atLeastOneSessionBuffer = false;
+        do {
+            bufs = new ByteBuffer[len];
+            for (int i = 0; i < len; i++) {
+                bufs[i] = switch (RANDOM.nextInt(3)) {
+                    case 0 -> {
+                        byte[] b = new byte[size];
+                        RANDOM.nextBytes(b);
+                        yield ByteBuffer.wrap(b);
+                    }
+                    case 1 -> {
+                        byte[] b = new byte[size];
+                        RANDOM.nextBytes(b);
+                        yield MemorySegment.ofArray(b).asByteBuffer();
+                    }
+                    case 2 -> {
+                        atLeastOneSessionBuffer = true;
+                        yield segmentBufferOfSize(session, size);
+                    }
+                    default -> throw new AssertionError("cannot happen");
+                };
+            }
+        } while (!atLeastOneSessionBuffer);
+        return bufs;
+    }
+
+    static void assertMessage(Exception ex, String msg) {
+        assertTrue("Expected [%s], in: [%s]".formatted(msg, ex.getMessage()), ex.getMessage().contains(msg));
+    }
+
+    static void assertCauses(Throwable ex, Class<? extends Exception>... exceptions) {
+        for (var expectedClass : exceptions) {
+            ex = ex.getCause();
+            assertTrue("Expected %s, got: %s".formatted(expectedClass, ex), expectedClass.isInstance(ex));
+        }
+    }
+
+    @DataProvider(format = "%m[%i]")
+    public static Object[][] confinedArenas() {
+        return new Object[][]{
+                {ArenaSupplier.NEW_CONFINED},
+        };
+    }
+
+    @DataProvider(format = "%m[%i]")
+    public static Object[][] sharedArenas() {
+        return new Object[][]{
+                {ArenaSupplier.NEW_SHARED},
+        };
+    }
+
+    @DataProvider(format = "%m[%i]")
+    public static Object[][] closeableArenas() {
+        return Stream.of(sharedArenas(), confinedArenas())
+                .flatMap(Arrays::stream)
+                .toArray(Object[][]::new);
+    }
+
+    @DataProvider(format = "%m[%i]")
+    public static Object[][] sharedArenasAndTimeouts() {
+        return new Object[][]{
+                {ArenaSupplier.NEW_SHARED, 0},
+                {ArenaSupplier.NEW_SHARED, 30},
+        };
+    }
+
+    static class ArenaSupplier implements Supplier<Arena> {
+
+        static final Supplier<Arena> NEW_CONFINED =
+                new ArenaSupplier(Arena::ofConfined, "confined arena");
+        static final Supplier<Arena> NEW_SHARED =
+                new ArenaSupplier(Arena::ofShared, "shared arena");
+
+        private final Supplier<Arena> supplier;
+        private final String str;
+
+        private ArenaSupplier(Supplier<Arena> supplier, String str) {
+            this.supplier = supplier;
+            this.str = str;
+        }
+
+        @Override
+        public String toString() {
+            return str;
+        }
+
+        @Override
+        public Arena get() {
+            return supplier.get();
+        }
+    }
+}
